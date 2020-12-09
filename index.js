1ebc3318408e370613ed6c65cba4484a8c24bf0f
JaswanthReddyPadala <jaswanthreddypadala@gmail.com>
2020-12-09 20:40:14 +0530

    add: load more option

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var accessKey = 'Xg9iCCp7u4viMa-tXn2fuobt7YPL_S_TOKpw-XfpnEQ';
+var accessKey = 'yFMKPaTXKn2X2qOyHeTuwyCXqeAj_Xx7KJLnIeXGGOM';
 //Xg9iCCp7u4viMa-tXn2fuobt7YPL_S_TOKpw-XfpnEQ
 // yFMKPaTXKn2X2qOyHeTuwyCXqeAj_Xx7KJLnIeXGGOM
 var searchUrl = `https://api.unsplash.com/search/photos?client_id=${accessKey}`; 
-var featuredUrl = `https://api.unsplash.com/photos?client_id=${accessKey}&per_page=20`; 
+var featuredUrl = `https://api.unsplash.com/photos?client_id=${accessKey}`; 
 var randomUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}`;
-var query = document.querySelector("#searchInput");
+var query = document.querySelector("#searchInput").value;
 var searchBtn = document.querySelector("#searchBtn");
 var headerImage = document.querySelector("header");
 var main = document.querySelector("main");
 var navItem = document.querySelectorAll(".nav-item");
+var loadBtn = document.querySelector(".loadmore p");
 var per = 20;
+var pageNum = 1;
 window.addEventListener("load", function randomPic(){
     fetch(randomUrl)
     .then(photos => photos.json())
@@ -21,7 +23,8 @@ window.addEventListener("load", function randomPic(){
 
 
 window.addEventListener("load", function onloadFeatured(){
-    fetch(featuredUrl)
+    pageNum=1;
+    fetch(featuredUrl+`&per_page=${per}&page=${pageNum}`)
     .then(photos => photos.json())
     .then(pics => {
             pics.forEach(pic => {
@@ -35,11 +38,11 @@ window.addEventListener("load", function onloadFeatured(){
 
 function navClick(index){
     console.log(navItem[index].innerHTML);
-    displayMain(navItem[index].innerHTML, main);
+    query = navItem[index].innerHTML;
+    displayMain(query, main);
 }
 searchBtn.addEventListener("click", function handleEvent(){
-    
-    displayMain(query.value, main);
+    displayMain(query, main);
 });
 
 function clearElement(main){
@@ -56,25 +59,64 @@ main.appendChild(errorMsg);
 
 function displayMain(value,main) {
     clearElement(main);
-    console.log(value, per)
-    fetch(searchUrl+`&query=${value}&per_page=${per}`)
+    pageNum=1;
+    fetch(searchUrl+`&query=${value}&per_page=${per}&page=${pageNum}`)
     .then(photos => photos.json())
     .then(pics => {
             pics["results"].forEach(pic => {
                 var imgelement = document.createElement('img');
                 imgelement.setAttribute("class","picture");
-                imgelement.setAttribute('src', pic["urls"]["thumb"]);
+                imgelement.setAttribute('src', pic["urls"]["regular"]);
                 main.appendChild(imgelement);
         });
     }).catch(handleError());
 }
-var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
 
+function loadmore(value){
+    pageNum +=1;
+    if (value === ""){
+        fetch(featuredUrl+`&query=${value}&per_page=${per}&page=${pageNum}`)
+    .then(photos => photos.json())
+    .then(pics => {
+            pics.forEach(pic => {
+                var imgelement = document.createElement('img');
+                imgelement.setAttribute("class","picture");
+                imgelement.setAttribute('src', pic["urls"]["thumb"]);
+                main.appendChild(imgelement);
+        });
+    })
+    }
+    else{
+        fetch(searchUrl+`&query=${value}&per_page=${per}&page=${pageNum}`)
+        .then(photos => photos.json())
+        .then(pics => {
+            pics["results"].forEach(pic => {
+                var imgelement = document.createElement('img');
+                imgelement.setAttribute("class","picture");
+                imgelement.setAttribute('src', pic["urls"]["regular"]);
+                main.appendChild(imgelement);
+            });
+        })
+    }
+}
+
+loadBtn.addEventListener("click", function handleLoadBtn(){
+    loadmore(query);
+});
+
+// var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+
+// window.onscroll = function (){
+//     if(window.pageYOffset > main.scrollHeight-300){
+//         console.log(window.pageYOffset, query);
+//         loadmore(query);
+//     }
+// }
 // window.addEventListener("scroll", function listenscroll(){
 //     var page_height = document.body.offsetHeight ? document.body.offsetHeight : document.height;
-//     if (main.scrollTop + main.clientHeight > main.scrollHeight){
-//         // per +=20; 
+//     if (main.scrollTop + main.clientHeight >= main.scrollHeight){
+//         per +=20; 
 //         alert("down");
-//         // displayMain(value);
+//         displayMain(value);
 //     }
 // })
